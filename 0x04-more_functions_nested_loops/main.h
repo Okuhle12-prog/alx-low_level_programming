91344923775801fd8ab5ca5b4bf5e9f7f9366c70
Okuhle12-prog <thandokuhledupont@gmail.com>
2022-12-15 08:48:30 -0800

    Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,12 @@
-#ifndef MAIN H
-#define MAIN H
+#ifndef MAIN_H
+#define MAIN_H
 
-int putchar(char c);
+int _putchar(char c);
 int _isupper(int c);
-int isdigit(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-void print_most_ numbers(void);
+void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
