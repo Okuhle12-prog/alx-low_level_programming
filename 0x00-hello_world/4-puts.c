33ed16d84487068b6da3a6082e02fa8fed257c15
Okuhle12-prog <thandokuhledupont@gmail.com>
2022-12-21 01:39:18 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Return: o
- *    */
+ * main - main block
+ * Return: o
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
